2b0334ece71b43c009f5c8eab82045284985abd0
Abdallah Ibrahim <iabdollah40@gmail.com>
2026-05-23 18:55:31 +0300

    Update kernel string in anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Khaenriah by @D_ai_n
+kernel.string=xxKhaenriah for r5p by @D_ai_n
 do.devicecheck=1
 do.modules=0
 do.systemless=1
